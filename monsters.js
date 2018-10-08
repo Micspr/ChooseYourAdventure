a53eeaddde5009b76056d060546ec60bce2e254a
Michael Spraggins <michael.c.spraggins@gmail.com>
2018-10-08 14:50:35 -0700

    Adjusted Monster special abilities.

--- monsters.js
+++ monsters.js
@@ -1,9 +1,9 @@
 const rollDice = requires('./math.js')
-const monsters = [{name: 'Brutalus', hp: 20, strength: rollDice(8), ability: 'Devour'/*Regen 5 HP*/, storyPath: 0},
+const monsters = [{name: 'Brutalus', hp: 20, strength: rollDice(8), ability: {devour: creatureInput.hp += 5}, storyPath: 0},
 //Schism Hydra
-{name: 'Proximo', hp: 35, strength: rollDice(8), ability: 'Breath'/*variable damage rollDice(3)^5*/, storyPath: 1},
+{name: 'Proximo', hp: 35, strength: rollDice(8), ability: {Breath: rollDice(3)*5}, storyPath: 1},
 // Chromatic Dragon
-{name: 'Xyligtl', hp: 50, strength: rollDice(4), ability: 'Consume'/*Instant loss event*/, storyPath: 2}]
+{name: 'Xyligtl', hp: 50, strength: rollDice(4), ability: {Consume:/*Select Loss Event*/'woops'}, storyPath: 2}]
 //Chaos Spawn
 const theWhiteRaven = {name: 'White Raven', hp: Infinity, strength: rollDice(40)*20}
 
