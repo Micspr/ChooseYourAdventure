7d52a26084d5d151d7b60f09db8d09ad6c0aaab4
Michael Spraggins <michael.c.spraggins@gmail.com>
2018-10-08 13:30:57 -0700

    Added opening line to story template. Added antagonists.

---
+++ monsters.js
@@ -0,0 +1,6 @@
+const theHydra = {name: 'Brutalus', hp: 20, strength: rollDice(8), ability: 'Devour'/*Regen 5 HP*/}
+//Schism Hydra
+const theDragon = {name: 'Proximo', hp: 35, strength: rollDice(8), ability: 'Breath'/*variable damage rollDice(3)^5*/}
+// Chromatic Dragon
+const theTerror = {name: 'Xyligtl', hp: 50, strength: rollDice(4), ability: 'Consume'/*Instant loss event*/}
+//Chaos Spawn
---
+++ storyTemplates.js
@@ -0,0 +1,1 @@
+const openingScript = `Gather and behold! The story of ${championName}, the ${championTitle} of ${championHeritage}! Will they succeed upon their quest and conquer the mighty ${creatureInput}? Will they fail and become another snack? Let's decide...`
