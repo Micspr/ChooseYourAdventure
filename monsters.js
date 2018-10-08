eec4dfce141c08215ce1a25dfdc00f9f94a5405c
Michael Spraggins <michael.c.spraggins@gmail.com>
2018-10-08 14:46:33 -0700

    Added the White Raven. Morphed monsters into an array of objects for better accessibility later. Added require statements and the basic monster selector.

--- monsters.js
+++ monsters.js
@@ -1,6 +1,10 @@
-const theHydra = {name: 'Brutalus', hp: 20, strength: rollDice(8), ability: 'Devour'/*Regen 5 HP*/}
+const rollDice = requires('./math.js')
+const monsters = [{name: 'Brutalus', hp: 20, strength: rollDice(8), ability: 'Devour'/*Regen 5 HP*/, storyPath: 0},
 //Schism Hydra
-const theDragon = {name: 'Proximo', hp: 35, strength: rollDice(8), ability: 'Breath'/*variable damage rollDice(3)^5*/}
+{name: 'Proximo', hp: 35, strength: rollDice(8), ability: 'Breath'/*variable damage rollDice(3)^5*/, storyPath: 1},
 // Chromatic Dragon
-const theTerror = {name: 'Xyligtl', hp: 50, strength: rollDice(4), ability: 'Consume'/*Instant loss event*/}
+{name: 'Xyligtl', hp: 50, strength: rollDice(4), ability: 'Consume'/*Instant loss event*/, storyPath: 2}]
 //Chaos Spawn
+const theWhiteRaven = {name: 'White Raven', hp: Infinity, strength: rollDice(40)*20}
+
+module.exports = monsters, theWhiteRaven;
--- storyTemplates.js
+++ storyTemplates.js
@@ -1,1 +1,4 @@
-const openingScript = `Gather and behold! The story of ${championName}, the ${championTitle} of ${championHeritage}! Will they succeed upon their quest and conquer the mighty ${creatureInput}? Will they fail and become another snack? Let's decide...`
+let creatureInput = monsters[rollDice(3)]
+
+const openingScript = `Gather and behold! The story of ${championName}, the ${championTitle} of ${championHeritage}! Will they succeed upon their quest and conquer the mighty ${creatureInput.name}? Will they fail and become another snack? Let's decide...`
+
